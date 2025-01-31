3ecd740020f86e9c34fd2314554a54d07c7a87be
sarthakdev143-lite <sattubhai272@gmail.com>
2025-02-01 00:09:11 +0530

    Add alert for project status in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import About from "@/components/About";
 import Landing from "@/components/Landing";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    alert("This Project is still under development. Some features may not work as expected. If You are still interested in exploring the project, please visit `https://sarthakdev-ruined.vercel.app`");
+  }, []);
+
   return (
     <>
       <Landing />
       <About />
-      <div className="w-full h-[100rem] bg-red-900"></div>
     </>
   );
 }
